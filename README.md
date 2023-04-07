24c12783bd6f60bcf826c6147cff79f818cdedaa
Claudiney Nascimento <contato@claudiney.info>
2023-04-07 16:45:47 -0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # quarkus-greeting-extension
+
+## building my first extension
 https://quarkus.io/guides/building-my-first-extension
+
+## writing-extensions
+https://quarkus.io/guides/writing-extensions
--- deployment/src/main/java/org/acme/greeting/extension/deployment/GreetingExtensionProcessor.java
+++ deployment/src/main/java/org/acme/greeting/extension/deployment/GreetingExtensionProcessor.java
@@ -2,6 +2,8 @@ package org.acme.greeting.extension.deployment;
 
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.undertow.deployment.ServletBuildItem;
+import org.acme.greeting.extension.GreetingExtensionServlet;
 
 class GreetingExtensionProcessor {
 
@@ -10,5 +12,14 @@ class GreetingExtensionProcessor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    ServletBuildItem createServlet() {
+        ServletBuildItem servletBuildItem = ServletBuildItem
+                .builder("greeting-extension", GreetingExtensionServlet.class.getName())
+                .addMapping("/greeting")
+                .build();
+        return servletBuildItem;
     }
 }
